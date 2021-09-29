5d2d84c309be5dd7c2cc2c49cf8db8d942353c97
ANR <anr.qwert@yandex.ru>
2021-09-29 17:30:49 +0300

    Editing because of warnings

--- vec_sum.cu
+++ vec_sum.cu
@@ -14,7 +14,7 @@ __global__ void vecAdd(double *A, double *B, double *C){
 }
 
 bool checkResults(double *A, double *B, double *res_gpu){
-	double *res_cpu = new double[n]
+	double *res_cpu = new double[n];
 	auto start = std::chrono::steady_clock::now();
 	for (int i = 0; i < n; ++i){
 		res_cpu[i] = A[i] + B[i];
@@ -23,19 +23,21 @@ bool checkResults(double *A, double *B, double *res_gpu){
 	std::chrono::duration<double> elapsed_seconds = end-start;
  	std::cout << "Time for CPU: " << elapsed_seconds.count() << "s\n";
 	int num_of_err = 0;
-	bool is_correct = True;
+	bool is_correct = 1;
 	for (int i = 0; i < n; ++i){
 		if (res_cpu[i] != res_gpu[i]){
 			num_of_err++;
 			std::cout << "Error in " << i + 1 << " element;\nOn CPU " << res_cpu[i] << ", On GPU " << res_gpu[i] << ";\n";
-			is_correct = False;
+			is_correct = 0;
 		}
 	}
 	if (is_correct){
 		std::cout << "Everything is great, results are equal!" << '\n';
+
 	} else{
 		std::cout << "There are " << num_of_err << " errors there. Hm... maybe we did something wrong..." << '\n';
 	}
+	return is_correct;
 }
 
 int main(int argc, char* argv[]){
@@ -45,7 +47,7 @@ int main(int argc, char* argv[]){
 	double *B = (double*)malloc(bytes);
 	double *res = (double*)malloc(bytes);
 	char decision;
-	std::std::cout << "Do you want to fill arrays by yourself? (Y/N)" << '\n';
+	std::cout << "Do you want to fill arrays by yourself? (Y/N)" << '\n';
 	std::cin >> decision;
 	switch(tolower(decision))
 	{
